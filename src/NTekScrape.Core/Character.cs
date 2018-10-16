9222f0a277da10cca2c906a29f75d9a85a3fa777
jamie <jamie.moreland@kcl.ac.uk>
2018-10-16 22:19:59 +0100

    Add character enums

--- src/NTekScrape.Core/Character.cs
+++ src/NTekScrape.Core/Character.cs
@@ -2,6 +2,6 @@
 {
     public enum Character
     {
-        Asuka
+        Akuma, Alisa, Anna, Asuka, Bob, Bryan, Claudio, DevilJin, Dragunov, Eddy, Eliza, Feng, Geese, Gigas, Heihachi, Hwoarang, Jack7, Jin, Josie, Katarina, Kazumi, Kazuya, King, Kuma, Lars, Lei, Law, Lee, Leo, Lili, LuckyChole, MasterRaven, Miguel, Nina, Noctis, Paul, Shaheen, Steve, Xiaoyu, Yoshimitsu
     }
 }
