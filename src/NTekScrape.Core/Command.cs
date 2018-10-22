1cdb2e37975d58ab8254d18779fb36738e088794
jamie <jamie.moreland@kcl.ac.uk>
2018-10-22 19:54:14 +0100

    Add command class

---
+++ src/NTekScrape.Core/Command.cs
@@ -0,0 +1,23 @@
+﻿using NTekScrape.Core.interfaces;
+
+namespace NTekScrape.Core
+{
+    class Command : ICommand
+    {
+        public string Input { get; set; }
+
+        public string HitLevel { get; set; }
+
+        public string Damage { get; set; }
+
+        public string StartUp { get; set; }
+
+        public string BlockFrame { get; set; }
+
+        public string HitFrame { get; set; }
+
+        public string CounterHitFrame { get; set; }
+
+        public string Properties { get; set; }
+    }
+}
