34571c3ea001e5d6d5d768605cd5906b59d4ab76
jamie <jamie.moreland@kcl.ac.uk>
2018-10-25 21:24:27 +0100

    Fix enum typo

--- src/NTekScrape.Core/Character.cs
+++ src/NTekScrape.Core/Character.cs
@@ -2,6 +2,6 @@
 {
     public enum Character
     {
-        Akuma, Alisa, Anna, Asuka, Bob, Bryan, Claudio, DevilJin, Dragunov, Eddy, Eliza, Feng, Geese, Gigas, Heihachi, Hwoarang, Jack7, Jin, Josie, Katarina, Kazumi, Kazuya, King, Kuma, Lars, Lei, Law, Lee, Leo, Lili, LuckyChole, MasterRaven, Miguel, Nina, Noctis, Paul, Shaheen, Steve, Xiaoyu, Yoshimitsu
+        Akuma, Alisa, Anna, Asuka, Bob, Bryan, Claudio, DevilJin, Dragunov, Eddy, Eliza, Feng, Geese, Gigas, Heihachi, Hwoarang, Jack7, Jin, Josie, Katarina, Kazumi, Kazuya, King, Kuma, Lars, Lei, Law, Lee, Leo, Lili, LuckyChloe, MasterRaven, Miguel, Nina, Noctis, Paul, Shaheen, Steve, Xiaoyu, Yoshimitsu
     }
 }
